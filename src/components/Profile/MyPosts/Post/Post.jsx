d3c383f908a00c63f1bb9783db4114952e30b6dc
Anton Kharchenko <gumb1t97@gmail.com>
2020-03-01 22:57:21 +0300

    lesson 19

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -3,18 +3,18 @@ import s from './Post.module.css';
 
 const Post = (props) => {
 
-  return (
-    <div className={s.item}>
-      <img src='https://movies4maniacs.liberty.me/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg' />
-         {props.lastname}
-         {props.name}
-         {props.likesCount}
-          <div>
-        <span>like</span>
-      </div>
-    </div>
+    return (
+        <div className={s.item}>
+            <img src='https://movies4maniacs.liberty.me/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg'/>
+            {props.lastname}
+            {props.name}
+            {props.likesCount}
+            <div>
+                <span>like</span>
+            </div>
+        </div>
 
-  )
+    )
 }
 
 export default Post;
