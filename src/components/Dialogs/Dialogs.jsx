fbc9f5ea773713bbedbeb63c266b3b1fa0f6185d
Anton Kharchenko <gumb1t97@gmail.com>
2020-03-02 16:47:19 +0300

    lesson 21 Верстаем страницу Диалогов

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,32 @@
-import  React from  'react';
-import  s from './Dialogs.module.css';
+import React from 'react';
+import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
-    return(
-       <div>
-           Dialogs
-       </div>
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogItem}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Anton
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+                <div className={s.dialog}>
+                    Vika
+                </div>
+                <div className={s.dialog}>
+                    Ksenia
+                </div>
+            </div>
+            <div className={s.message}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your it-kamasutra?</div>
+                <div className={s.message}>Yo</div>
+            </div>
+        </div>
     );
 
 };
