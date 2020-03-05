ae2b514e1c4c922e968f3c4075a831cb8cf12b90
Gumb1t <anonymousStyleKiller>
2020-03-05 13:15:27 +0200

    31

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,24 +2,27 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
-const MyPosts = (props) => {
-    let postsData = [
-        {id: "1", name: "Anton", lastName: "Kharchenko", likesCount: "0"},
-        {id: "2", name: "Alex", lastName: "Glinka", likesCount: "23"}
-    ];
 
-    
-    let postsItem = postsData.map((post)=><Post  name={post.name} lastName={post.lastName} likesCount={post.likesCount}/>);
+const MyPosts = (props) => {
+
+    let postsItem = props.postsData.map((post) => <Post name={post.name} lastName={post.lastName}
+                                                        likesCount={post.likesCount}/>);
+
+    let newPostElement = React.createRef();
+    let addPost = () =>{
+       let text = newPostElement.current.value;    
+       alert(text);
+    }
 
     return (
         <div className={s.postsBlock}>
             <h3> My posts </h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
