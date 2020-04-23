38c81ea95440511a33b1b80adb686c1c58059a31
Anton Kharchenko <gumb1t97@gmail.com>
2020-04-23 17:09:26 +0300

    85 - useEffect

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,35 +1,37 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import css from './ProfileInfo.module.css';
 
 const ProfileStatusWithHooks = (props) => {
 
     const [state, setState] = useState(false);
     const [status, setStatus] = useState(props.status);
-
-   const activatedMode = () =>{
-       setState(true);
+    useEffect(()=>{
+        setStatus(props.status);
+    }, [props.status])
+    const activatedMode = () => {
+        setState(true);
     }
 
-    const deActivatedMode = () =>{
+    const deActivatedMode = () => {
         setState(false);
         props.updateStatus(status);
     }
 
     const onStatusChange = (e) => {
-       setStatus(e.currentTarget.value);
+        setStatus(e.currentTarget.value);
     };
 
     return (
         <div>
-            {!state&&
+            {!state &&
             <div>
-                        <span  onDoubleClick={activatedMode} className={css.status}>
+                        <span onDoubleClick={activatedMode} className={css.status}>
                             <div>Your status: &nbsp;</div>
                             <div className={css.meStatus}>{props.status || "No Status"}</div>
                         </span>
             </div>
             }
-            {state&&
+            {state &&
             <div className={css.status}>
                 <div>Your status: &nbsp;</div>
                 <input onChange={onStatusChange} onBlur={deActivatedMode} autoFocus={true}
